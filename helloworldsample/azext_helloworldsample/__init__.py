3ef56f248b9ea31bcfd1b814858d99ff61164315
Pengzhi Sun <pengzhisun@live.com>
2017-11-30 13:17:03 +0800

    update hello world module comments

--- helloworldsample/azext_helloworldsample/__init__.py
+++ helloworldsample/azext_helloworldsample/__init__.py
@@ -3,24 +3,32 @@
 # Summary:
 #    Defines the Azure CLI extension commands.
 # See: 
-#    https://packaging.python.org/tutorials/distributing-packages/#setup-py
-#    https://github.com/pypa/sampleproject/blob/master/setup.py
-#    https://packaging.python.org/en/latest/distributing.html
+#    https://github.com/Azure/azure-cli/blob/master/doc/extensions/authoring.md
 # ==============================================================================
 
 from azure.cli.core.help_files import helps
 
+# Set command help message
+# See:
+#    https://github.com/Azure/azure-cli/blob/master/doc/authoring_help.md
 helps['hello world'] = """
     type: command
     short-summary: Say hello world.
 """
 
+# The command operator method
 def helloworld():
     print('Hello World.') #pylint: disable-msg=C0325
 
+# Set the command arguments
+# See:
+#    https://github.com/Azure/azure-cli/blob/master/doc/authoring_command_modules/authoring_commands.md
 def load_params(_):
     pass
 
+# Registering Commands
+# See:
+#    https://github.com/Azure/azure-cli/blob/master/doc/authoring_command_modules/authoring_commands.md
 def load_commands():
     from azure.cli.core.commands import cli_command
     cli_command(__name__, 'hello world', 'azext_helloworldsample#helloworld')
