92f55f54297efecbdf0013e6f4700f05283ce585
Pengzhi Sun <pengzhisun@live.com>
2017-11-30 12:13:58 +0800

    Initial project
    Add hello world sample

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # azure-cli-extension-sample
-Azure CLI 2.0 extension sample
+This is a sample project to show how to [author an Azure CLI 2.0 extension](https://github.com/Azure/azure-cli/blob/master/doc/extensions/authoring.md).
+
+## Usages:
+
+1. navigate to the package folder:
+```{r, engine = 'bash', eval = FALSE}
+    cd helloworldsample
+```
+2. build python wheel package:
+```{r, engine = 'bash', eval = FALSE}
+    python3 setup.py bdist_wheel
+```
+3. add azure cli extension from local file:
+```{r, engine = 'bash', eval = FALSE}
+    az extension add --source ./dist/helloworldsample-0.0.1-py2.py3-none-any.whl -y
+```
+4. verify extension worked
+```{r, engine = 'bash', eval = FALSE}
+    az hello world
+```
+5. remove azure cli extension
+```{r, engine = 'bash', eval = FALSE}
+    az extension remove -n "helloworldsample"
+```
---
+++ helloworldsample/azext_helloworldsample/__init__.py
@@ -0,0 +1,26 @@
+# ==============================================================================
+# File Name:    __init__.py
+# Summary:
+#    Defines the Azure CLI extension commands.
+# See: 
+#    https://packaging.python.org/tutorials/distributing-packages/#setup-py
+#    https://github.com/pypa/sampleproject/blob/master/setup.py
+#    https://packaging.python.org/en/latest/distributing.html
+# ==============================================================================
+
+from azure.cli.core.help_files import helps
+
+helps['hello world'] = """
+    type: command
+    short-summary: Say hello world.
+"""
+
+def helloworld():
+    print('Hello World.') #pylint: disable-msg=C0325
+
+def load_params(_):
+    pass
+
+def load_commands():
+    from azure.cli.core.commands import cli_command
+    cli_command(__name__, 'hello world', 'azext_helloworldsample#helloworld')
